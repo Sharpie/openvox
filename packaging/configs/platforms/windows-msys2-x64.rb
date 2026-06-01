420c88369e6f7acf0cc4ecb156c796a90da9ae3a
Charlie Sharpsteen <charlie@overlookinfratech.com>
2026-05-31 20:33:10 -0500

    Add windows-msys2-x64 platform definition
    This commit adds a `windows-msys2-x64` platform definition that is
    mostly a copy of the existing Cygwin-based `windows-all-x64`.
    Vanagon builds that use this platform should be run inside a MSYS2
    bash shell and use the `MSYS/ruby` package to run Vanagon. This provides
    a Cygwin-ish, POSIX-ish, execution environment that Vanagon expects
    and that somewhat mirrors the Cygwin environment used by the existing
    `windows-all-x64` platform. Do not use the `UCRT64/ruby` package to
    run Vanagon as this is "Windows native" instead of POSIX and will try
    to use Windows paths, Windows line endings, and importantly will also
    try to run Vanagon shell commands through `cmd.exe` instead of
    `bash.exe`.
    
    The Vanagon platform is configured to install the `UCRT64/gcc` package
    and components are set to build Windows native executables linked
    against the `ucrt` C runtime required by Ruby 4.0.
    
    This commit is based on work by Sebastian Rakel:
    
      https://github.com/OpenVoxProject/puppet-runtime/commit/622973629bdf6138b942745c65a8ad313fede508
    
    Co-Authored-By: Sebastian Rakel <sebastian@devunit.eu>
    Signed-off-by: Charlie Sharpsteen <charlie@overlookinfratech.com>

---
+++ packaging/configs/platforms/windows-msys2-x64.rb
@@ -0,0 +1,46 @@
+platform 'windows-msys2-x64' do |plat|
+  # Not currently used
+  plat.docker_image 'windows:ltsc2019'
+  plat.docker_registry 'mcr.microsoft.com'
+  plat.docker_arch 'linux/amd64'
+
+  plat.servicetype 'windows'
+
+  packages = %w[
+    autoconf
+    git
+    make
+    patch
+    mingw-w64-ucrt-x86_64-gcc
+    mingw-w64-ucrt-x86_64-gdbm
+    mingw-w64-ucrt-x86_64-zlib
+    mingw-w64-ucrt-x86_64-gcc-libs
+  ]
+
+  # Story time:
+  #
+  # Windows can end up with multiple MSYS2 installations. This is common
+  # on GitHub actions where the runner image comes with `C:/msys64/` 
+  # pre-installed, but inactive, and the `msys/setup-msys2` creates a new
+  # installation under `D:/a/_temp/msys64/`.
+  #
+  # Thus: assume Vanagon is running in a MSYS2 shell which mounts the chosen
+  # MSYS2 installation and sets PATH appropriately. "UNIX" paths in this shell,
+  # like `/usr/bin/` should work fine, but fully-qualified Windows paths like
+  # `C:/msys64/usr/bin/` might point to the "wrong" MSYS2 installation.
+  plat.provision_with("/usr/bin/pacman -S --noconfirm --needed #{packages.join(' ')}")
+  plat.install_build_dependencies_with '/usr/bin/pacman -S --noconfirm --needed'
+
+  plat.make '/usr/bin/make'
+  plat.patch 'TMP=/var/tmp /usr/bin/patch.exe --binary'
+
+  plat.platform_triple 'x86_64-w64-mingw32'
+
+  # Putting these here as a reminder where we use them elsewhere. DO NOT
+  # use the full path, just the name of the executable without the extension.
+  # Otherwise, autoconf gets confused.
+  plat.environment 'CC', 'x86_64-w64-mingw32-gcc'
+
+  plat.package_type 'msi'
+  plat.output_dir 'windows'
+end
